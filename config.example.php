f037ceaabe9deb701117e221314c9166eb544616
mraiur <mraiur@gmail.com>
2015-03-31 14:50:11 +0300

    Example config

---
+++ config.example.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'meta-title' => 'Meta title',
+    'meta-description' => 'meta description',
+    'meta-keywords' => 'keywords',
+
+    'name' => "Firstname Lastname",
+    'short-description' => 'Ultimate developer',
+
+    'facebook' => 'Facebook link',
+    'twitter' => 'Twitter link',
+    'google-plus' => 'Google plus link',
+    'linkedin' => 'LinkedIn link'
+];
